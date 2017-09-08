8f511fb2caf79b5745d8b3a58515d5c9a92a8909
Pascal <pascal.morin@codeenigma.com>
2017-09-08 16:50:26 +0200

    Adding cache for apt

--- Docker/export.sh
+++ Docker/export.sh
@@ -3,6 +3,22 @@
 # (Re)build a Docker base box.
 #
 
+usage(){
+  cat << EOF
+usage:
+
+Export a base CodeEnigma image.
+$0 <versiontag>
+
+EOF
+}
+
+# Quick check we have args.
+if [ -z $1 ]; then
+  usage
+  exit 1;
+fi
+
 # Keep current dir in mind to know where to move back when done.
 OWN=$(readlink "$0");
 if [ -z "$OWN" ]; then
@@ -11,7 +27,7 @@ fi
 OWN_DIR=$( cd "$( dirname "$OWN" )" && pwd -P)
 
 echo "1. Building the image"
-docker image build --compress --label=jessie64 --no-cache=true -t pmce/jessie64 "$OWN_DIR" || exit 1
+docker image build --compress --label=jessie64 --no-cache=true -t pmce/jessie64:$1 "$OWN_DIR" || exit 1
 
 echo "Image is ready to be published with 'docker image push pmce/jessie64'"
 echo "Do not forget to increment the version tag"
