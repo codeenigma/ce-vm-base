d0c51b66f59c042ab1d5ab5591512cd5eb6d6e06
Pascal <pascal.morin@codeenigma.com>
2018-02-18 16:52:22 +0100

    Uncomment all jobs

--- Docker/export.sh
+++ Docker/export.sh
@@ -3,7 +3,7 @@
 # (Re)build a Docker base box for ce-vm.
 #
 
-IMAGES="db"
+IMAGES="log app db"
 
 usage(){
   cat << EOF
@@ -32,9 +32,9 @@ OWN_DIR=$( cd "$( dirname "$OWN" )" && pwd -P)
 docker image pull debian:jessie-slim
 # Build base image.
 echo "1. Building the image"
-#docker image build --compress --label=ce-vm:$1 --no-cache=true -t "pmce/ce-vm:$1" "$OWN_DIR/base" || exit 1
+docker image build --compress --label=ce-vm:$1 --no-cache=true -t "pmce/ce-vm:$1" "$OWN_DIR/base" || exit 1
 echo "Publishing the image with docker image push pmce/ce-vm:$1"
-#docker image push "pmce/ce-vm:$1"
+docker image push "pmce/ce-vm:$1"
 # Build all images recursively.
 for IMAGE in $IMAGES; do
   docker image pull "pmce/ce-vm:$1"
