c084fcf9ce8f81a8c5e87f4a4a8d5b838260994e
pascal <pascal.morin@codeenigma.com>
2018-04-12 17:06:08 +0200

    Wrong name for node image

--- Docker/export.sh
+++ Docker/export.sh
@@ -60,7 +60,7 @@ IMAGES="base-php log memcached mkdocs mysql nginx redis solr"
 build_images 'ce-vm' "$IMAGES" "$1"
 
 # NodeJS images built from the base.
-IMAGES="node-4.x node-6.x node-8.x node-9.x"
+IMAGES="nodejs-4.x nodejs-6.x nodejs-8.x nodejs-9.x"
 build_images 'ce-vm' "$IMAGES" "$1"
 
 # Derivatives from base-php.
